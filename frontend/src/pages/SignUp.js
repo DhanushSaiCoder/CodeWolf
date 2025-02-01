6b7b610757337ffded604ef1a7591bbfbe35f053
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-02 00:41:56 +0530

    signUp style

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,7 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
+import "../styles/SignUp.css";
+import codeWolf from '../images/codeWolf.jpg';
 
-const SignUp = (props) => {
-    return ( <>SignUp Page</> );
+const SignUp = () => {
+    return (
+        <div className='SignUp'>
+            <div className='brandingDiv'>
+                <img className='signupLogo' src={codeWolf} alt="logo" />
+                <h1 className='signUpH1'>Code Wolf</h1>
+                <p className='motto'><i>Every skilled coder must master fixing mistakes!</i></p>
+            </div>
+
+            <div className='signupFormDiv'>
+                <h2>Join the Pack</h2>
+                <form>
+                    <div className="inputGroup">
+                        <input
+                            autoComplete='off'
+                            id="username"
+                            className='inputEle'
+                            type="text"
+                            placeholder=" "
+                        />
+                        <label htmlFor="username">Username</label>
+                    </div>
+
+                    <div className="inputGroup">
+                        <input
+                            autoComplete='off'
+
+                            id="email"
+                            className='inputEle'
+                            type="email"
+                            placeholder=" "
+                        />
+                        <label htmlFor="email">Email</label>
+                    </div>
+
+                    <div className="inputGroup">
+                        <input
+                            autoComplete='off'
+
+                            id="password"
+                            className='inputEle'
+                            type="password"
+                            placeholder=" "
+                        />
+                        <label htmlFor="password">Password</label>
+                    </div>
+
+                    <div className="inputGroup">
+                        <input
+                            autoComplete='off'
+
+                            id="confirmPassword"
+                            className='inputEle'
+                            type="password"
+                            placeholder=" "
+                        />
+                        <label htmlFor="confirmPassword">Confirm Password</label>
+                    </div>
+
+                    <button type="submit">Become a Wolf</button>
+                </form>
+            </div>
+        </div>
+    );
 }
- 
+
 export default SignUp;
