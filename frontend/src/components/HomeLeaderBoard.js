ddc1f72bd1037fbbb19af212544a925feebaa408
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-02 20:12:34 +0530

    placed leaderboard

---
+++ frontend/src/components/HomeLeaderBoard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import "../styles/HomeLeaderBoard.css"
+
+export const HomeLeaderBoard = () => {
+    return (
+        <div className='HomeLeaderBoard'>
+            Leaderboard
+        </div>
+    );
+}
