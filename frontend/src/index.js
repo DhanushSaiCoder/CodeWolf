2549e0876265a4bdc084dc0ce2b8afb1c4277a51
DhanushSaiCoder <149780956+DhanushSaiCoder@users.noreply.github.com>
2025-02-02 08:32:07 +0530

    Revert "Frontend"

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/auth/login" element={<Login />} />
-      <Route path="/auth/signup" element={<SignUp />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
