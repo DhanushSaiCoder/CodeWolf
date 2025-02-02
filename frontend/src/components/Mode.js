e64923410ffd3bcd66364a33882e7e3e44f2902e
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-02 19:47:14 +0530

    final mode styles

--- frontend/src/components/Mode.js
+++ frontend/src/components/Mode.js
@@ -2,24 +2,23 @@ import React from 'react';
 import "../styles/Mode.css";
 
 export const Mode = (props) => {
-    const { modeName, modeImg,modeImg2, modeDescription } = props;
+    const { modeName, modeImg, modeImg2, modeDescription } = props;
     return (
         <div className='mode'>
             <h2>{modeName}</h2>
             <div className='imgsDiv'>
                 <img className='imgLeft' src={modeImg} alt="modeImg" />
-                {/* <img className='imgCenter' src={hands} alt="handsImg" />
-                 */}
-                 <h3>{'< vs >'}</h3>
+                <h3>{'< vs >'}</h3>
                 <img className='imgRight' src={modeImg2} alt="modeImg" />
+            </div>
+            <div className='modeDescriptionDiv'>
+                <p className='modeDescriptionText'><i>{modeDescription}</i></p>
             </div>
             <div className='modeBtnsDiv'>
                 <button className='modeBtns quickMatchBtn'><b>QUICK MATCH</b></button>
                 <button className='modeBtns inviteFriendBtn'><b>INVITE FRIEND</b></button>
             </div>
-            <div className='modeDescriptionDiv'>
-                <p className='modeDescriptionText'><i>{modeDescription}</i></p>
-            </div>
+            
         </div>
     );
 };
