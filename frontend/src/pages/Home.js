f9194e72d12bba8f4c12c64ed89e01243ebab233
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-02 12:10:44 +0530

    nav background

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,10 +2,12 @@ import React, { useEffect } from 'react';
 import '../styles/Home.css'
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+
 const Home = () => {
     useEffect(() => {
         if(!localStorage.getItem('token')) window.location.href = '/auth/login'
     }, [])
+    
     return (
         <div className='Home'>
             <Header />
