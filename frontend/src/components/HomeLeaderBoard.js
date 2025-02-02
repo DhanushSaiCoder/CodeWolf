2c00e32a2b2b7bcb52bdcd536bae70fa9cf2c175
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-02 22:59:20 +0530

    leaderboard showMore

--- frontend/src/components/HomeLeaderBoard.js
+++ frontend/src/components/HomeLeaderBoard.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
-import "../styles/HomeLeaderBoard.css"
+import React from 'react';
+import "../styles/HomeLeaderBoard.css";
+import bronze from '../images/bronze.png';
+import gold from '../images/gold.png';
+import silver from '../images/silver.png';
+import { useNavigate } from 'react-router-dom';
 
 export const HomeLeaderBoard = () => {
-
+    const navigate = useNavigate()
     let data = [
         {
             username: 'Dhanush Sai',
@@ -43,44 +47,43 @@ export const HomeLeaderBoard = () => {
         {
             username: "Vikram",
             rating: 1178
-        },
-        ,
-        {
-            username: "Kavya",
-            rating: 1210
-        },
-        {
-            username: "Amit",
-            rating: 1193
-        },
-        {
-            username: "Sneha",
-            rating: 1157
-        },
-        {
-            username: "Vikram",
-            rating: 1178
         }
     ];
-    data = data.sort((a, b) => b.rating - a.rating)
-    console.log('data', data)
-    return (
-        <div className='HomeLeaderBoard'>
-            <div className='HomeLeaderBoardHeader'>
-                <h2>Leaderboard</h2>
-            </div>
-            <div className='HomeLeaderBoardContent'>
 
-                {data.map((u, index) => {
-                    return (
-                        <div className='lbUser'>
-                            <p>{index + 1}</p>
-                            <p><b>{u.username}</b></p>
-                            <p>{u.rating}</p>
-                        </div>
-                    )
-                })}
+    const sortByRating = (data) => {
+        return data.sort((a, b) => b.rating - a.rating);
+    };
+
+    data = sortByRating(data);
+    console.log('data', data);
+
+    const medals = [gold, silver, bronze];
+
+    return (
+        <div className='homeLeaderBoardContainer'>
+            <div className='HomeLeaderBoard'>
+                <div className='HomeLeaderBoardHeader'>
+                    <h2>Leaderboard</h2>
+                </div>
+                <div className='homeLeaderBoardContent'>
+                    {data.map((u, index) => (
+                        index < 10 ? (
+                            <div className={`lbUser ${index === 0 ? 'gold' : index === 1 ? 'silver' : index === 2 ? 'bronze' : ''}`} key={index}>
+                                <p>{index + 1}</p>
+                                <p><b>{u.username}</b></p>
+                                {index < 3 ? <img className='hlbBadge' height="35px" width="35px" src={medals[index]} alt={`${index + 1} place`} /> : <></>}
+                                <div className='hlbRating'>
+
+                                    <p><span className='starSymbol'>&#8902; </span>{u.rating}</p>
+                                </div>
+                            </div>
+                        ) : null
+                    ))}
+                    <button className='showMoreBtn' onClick={() => {
+                        navigate('/leaderboard')
+                    }}>Show More</button>
+                </div>
             </div>
         </div>
     );
-}
+};
