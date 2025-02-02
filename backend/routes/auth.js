ed168ed819012cc6ba596411c82983b6be0fa834
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-02 10:02:48 +0530

    before animation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const {validateUser, User} = require('../models/User')
+const { validateUser, User } = require('../models/User')
 
 router.get('/signup', (req, res) => {
     res.send('signup page');
@@ -15,19 +15,18 @@ router.get('/login', (req, res) => {
 
 // Register
 router.post('/signup', async (req, res) => {
-    // Validate the data before creating a user
+    // **Data Validation**
     const { error } = validateUser(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
-    // Check if the user is already in the database
+    // **Email Uniqueness Check**
     const emailExists = await User.findOne({ email: req.body.email })
-    if (emailExists) return res.status(400).send('Email already exists')
+    if (emailExists) return res.status(400).send({message:'Email already exists'})
 
-    // Hash the password
+    // **Password Hashing and User Creation**
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
-    // Create a new user
     const user = new User({
         username: req.body.username,
         email: req.body.email,
@@ -36,7 +35,7 @@ router.post('/signup', async (req, res) => {
     })
 
     try {
-        // Save the user to the database
+        // **Saving User to Database**
         const savedUser = await user.save()
         res.status(200).send(savedUser)
     } catch (err) {
@@ -44,11 +43,12 @@ router.post('/signup', async (req, res) => {
     }
 })
 
+
 // Login
 router.post('/login', async (req, res) => {
     // Validate the data before logging in a user
     if (!req.body.email || !req.body.password) return res.status(400).send('Email and password are required')
-        
+
     // Check if the email exists
     const user = await User.findOne({ email: req.body.email })
     if (!user) return res.status(400).send('Email or password is wrong')
