c1e9509a55d4b07a35ed4cb6aead3e6c212df88a
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-03 21:28:38 +0530

    Enhance FriendList component layout and styling for better usability

--- frontend/src/components/FriendList.js
+++ frontend/src/components/FriendList.js
@@ -2,19 +2,19 @@ import React from 'react';
 import "../styles/FriendList.css";
 
 export const FriendList = (props) => {
-  return (
-    <div className='FriendList'>
-      <div className='friendListHeader'>
-        <h1>FRIENDS</h1>
-        <div className='inputDiv'>
-            <input className='searchInp' type='search' placeholder='Search by username...' />
-            <label htmlFor='onlineCheckBox'>
-                <input id='onlineCheckBox' type="checkbox" />Online only
-            </label>
+    return (
+        <div className='FriendList'>
+            <div className='friendListHeader'>
+                <h1>FRIENDS</h1>
+                <div className='inputDiv'>
+                    <input className='searchInp' type='search' placeholder='Search by username...' />
+                    <label className='checkBoxLabel' htmlFor='onlineCheckBox'>
+                        <input id='onlineCheckBox' className='onlineCheckBox' type="checkbox" />Online only
+                    </label>
+                </div>
+
+                <button>Invite Friend</button>
+            </div>
         </div>
-        
-        <button>Invite Friend</button>
-      </div>
-    </div>
-  );
+    );
 }
