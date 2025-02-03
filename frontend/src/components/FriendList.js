1004292aa8d4146a7262403f76692aca1913e744
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-03 21:01:59 +0530

    before styling

--- frontend/src/components/FriendList.js
+++ frontend/src/components/FriendList.js
@@ -2,9 +2,16 @@ import React from 'react';
 import "../styles/FriendList.css";
 
 export const FriendList = (props) => {
-    return (
-        <div className='FriendList'>
-            FriendList
-        </div>
-    );
+  return (
+    <div className='FriendList'>
+      <div className='friendListHeader'>
+        <h1>FRIENDS</h1>
+        <input type='search' placeholder='Search by username' />
+        <label htmlFor='onlineCheckBox'>
+          <input id='onlineCheckBox' type="checkbox" />Online only
+        </label>
+        <button>Invite Friend</button>
+      </div>
+    </div>
+  );
 }
