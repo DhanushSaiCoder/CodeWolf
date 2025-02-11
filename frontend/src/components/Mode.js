3452ff44f8725f720ae4b21aac71f396cb5470ac
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-11 08:09:59 +0530

    Add navigation to 'Invite Friend' button in Mode component

--- frontend/src/components/Mode.js
+++ frontend/src/components/Mode.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import "../styles/Mode.css";
 
 export const Mode = (props) => {
+    const navigate = useNavigate();
     const { modeName, modeImg, modeImg2, modeDescription } = props;
+
     return (
         <div className='mode'>
             <h2>{modeName}</h2>
             <div className='imgsDiv'>
-                <img className='imgLeft' src={modeImg} alt="modeImg" />
+                <img className='imgLeft' src={modeImg} alt="Mode representation" />
                 <h3>{'< vs >'}</h3>
-                <img className='imgRight' src={modeImg2} alt="modeImg" />
+                <img className='imgRight' src={modeImg2} alt="Mode representation" />
             </div>
             <div className='modeDescriptionDiv'>
                 <p className='modeDescriptionText'><i>{modeDescription}</i></p>
             </div>
             <div className='modeBtnsDiv'>
                 <button className='modeBtns quickMatchBtn'><b>QUICK MATCH</b></button>
-                <button className='modeBtns inviteFriendBtn'><b>INVITE FRIEND</b></button>
+                <button
+                    onClick={() => navigate('/friends')}
+                    className='modeBtns inviteFriendBtn'
+                >
+                    <b>INVITE FRIEND</b>
+                </button>
             </div>
-            
         </div>
     );
 };
