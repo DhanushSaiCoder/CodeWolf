560e43d08698a11d515f62798f502ba03d1c33e7
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-24 17:53:25 +0530

    Add CodeEditor component to MatchRightColumn and adjust styles for layout consistency

---
+++ frontend/src/components/CodeEditor.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/CodeEditor.css"
+
+export default function CodeEditor() {
+  return (
+    <div className='CodeEditor'>
+      
+    </div>
+  )
+}
--- frontend/src/components/MatchRightColumn.js
+++ frontend/src/components/MatchRightColumn.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import "../styles/MatchRightColumn.css"
+import CodeEditor from './CodeEditor';
+
 export default function MatchRightColumn(props) {
   const { matchDoc } = props;
   return (
-    <div className='MatchRightColumn'>{matchDoc}</div>
+    <div className='MatchRightColumn'>
+      <CodeEditor matchDoc={matchDoc} />
+    </div>
   )
 }
