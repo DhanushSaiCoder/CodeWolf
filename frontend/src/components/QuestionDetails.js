ce987e6c40a59be8c428a2591b848763155c7384
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-24 17:15:54 +0530

    Add sample question data to QuestionDetails component and update CSS for improved scrolling behavior

--- frontend/src/components/QuestionDetails.js
+++ frontend/src/components/QuestionDetails.js
@@ -1,5 +1,20 @@
 import React from 'react'
-import  "../styles/QuestionDetails.css"
+import "../styles/QuestionDetails.css"
+
+const sampleQuestion = {
+  questionTitle: "Two Sum",
+  description: `Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.`,
+  examples: [
+    {
+      input: "nums = [2,7,11,15], target = 9",
+      output: [0, 1]
+    },
+    {
+      input: "nums = [3,2,4], target = 6",
+      output: [1, 2]
+    }
+  ]
+}
 
 export default function QuestionDetails() {
   return (
