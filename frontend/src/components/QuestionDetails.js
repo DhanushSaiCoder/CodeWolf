8a371c2469c1fd9c2de2d5279a91a0e432d09610
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-24 17:10:17 +0530

    Add header to QuestionDetails component with styling for improved presentation

--- frontend/src/components/QuestionDetails.js
+++ frontend/src/components/QuestionDetails.js
@@ -3,6 +3,10 @@ import  "../styles/QuestionDetails.css"
 
 export default function QuestionDetails() {
   return (
-    <div className='QuestionDetails'>QuestionDetails</div>
+    <div className='QuestionDetails'>
+      <div className='QuestionDetailsHeader'>
+        <h3>Question</h3>
+      </div>
+    </div>
   )
 }
