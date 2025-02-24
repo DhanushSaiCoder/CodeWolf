1137635ca4160f63ba11eab6c0296d1c4f27637e
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-24 16:33:27 +0530

    Add Timer component with styles; update MatchInfo and MatchLeftColumn styles

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import "../styles/Timer.css"
 
 export default function Timer() {
   return (
-    <div>Timer</div>
+    <div className='Timer'>
+        <div className='sliderBackground'>
+            <div className='slider'>
+                <p className='time'>24:36</p>
+            </div>
+        </div>
+    </div>
   )
 }
