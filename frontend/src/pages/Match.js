2eab907d2cf320143972133d46fa75d49dedf42f
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-24 10:57:44 +0530

    Add match retrieval endpoint and implement match fetching in Match component

--- frontend/src/pages/Match.js
+++ frontend/src/pages/Match.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import { get } from 'mongoose';
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
 
+const useQuery = () => new URLSearchParams(useLocation().search);
 export default function Match() {
+  //function to get the match doc with the match id from the url params.
+  const query = useQuery();
+  const matchId = query.get('matchId');
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/matches/${matchId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('matchDoc: ', data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  })
+
+
   return (
     <div>Match</div>
   )
+
 }
+
