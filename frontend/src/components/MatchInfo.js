fff03e0aa2eb927e884c06a3cef7037a3daffe42
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-02-24 16:12:06 +0530

    Refactor MatchInfo component to handle match data parsing and display player details; update styles for improved layout

--- frontend/src/components/MatchInfo.js
+++ frontend/src/components/MatchInfo.js
@@ -1,8 +1,41 @@
-import React from 'react'
-import "../styles/MatchInfo.css"
-export default function MatchInfo(props) {
-    const { matchDoc } = props;
+import React from 'react';
+import "../styles/MatchInfo.css";
+import Profile from "../images/profile.jpg";
+
+export default function MatchInfo({ matchDoc }) {
+    if (matchDoc == "null") return <div className='MatchInfo'>Loading...</div>;
+
+    let matchData;
+    try {
+        matchData = JSON.parse(matchDoc);
+    } catch (error) {
+        console.error("Error parsing matchDoc:", error);
+        return <div className='MatchInfo'>Invalid Match Data</div>;
+    }
+
     return (
-        <div className='MatchInfo'>{matchDoc}</div>
-    ) 
+        <div className='MatchInfo'>
+            <div className='MIPlayerDetails'>
+                {/* Player One */}
+                <div className='MIPlayerOneDetails bothPlayerDetails'>
+                    <img className='profileImgs' src={Profile} alt="playerOne" width={60} height={60} />
+                    <div>
+                        <h2>{matchData.players[0]?.username || "Unknown"}</h2>
+                        <p>★ {matchData.players[0]?.rating || "Unknown"}</p>
+                    </div>
+                </div>
+
+                {/* Player Two */}
+                <div className='MIPlayerTwoDetails bothPlayerDetails '>
+                    <div>
+
+                        <h2>{matchData.players[1]?.username || "Unknown"}</h2>
+                        <p>★ {matchData.players[1]?.rating || "Unknown"}</p>
+
+                    </div>
+                    <img className='profileImgs' src={Profile} alt="playerTwo" width={60} height={60} />
+                </div>
+            </div>
+        </div>
+    );
 }
