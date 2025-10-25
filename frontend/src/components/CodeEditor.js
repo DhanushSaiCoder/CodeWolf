5745cb677a8faeceecef2bc9cb370d4399ee4f48
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-10-25 12:42:09 +0530

    +submit btn

--- frontend/src/components/CodeEditor.js
+++ frontend/src/components/CodeEditor.js
@@ -110,7 +110,10 @@ export default function CodeEditor({ question, matchDoc: matchObj, handleCodeOut
         <h3>
           <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-code-xml-icon lucide-code-xml"><path d="m18 16 4-4-4-4" /><path d="m6 8-4 4 4 4" /><path d="m14.5 4-5 16" /></svg></span> Code
         </h3>
-        <button onClick={handleRunCode} className='runBtn'>Run</button>
+        <div className='code_editor_header_buttonsDiv'>
+          <button onClick={handleRunCode} className='runBtn'>Run</button>
+          <button className='submitBtn'>Submit</button>
+        </div>
       </div>
       <div className='CodeEditorContent'>
         {question != null ? <MonacoEditor
