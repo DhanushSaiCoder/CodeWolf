5de5a9a64adcbe09d66ea51c81ef201f4329a272
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-10-25 09:58:29 +0530

    send code and que id to the response

--- backend/routes/run.routes.js
+++ backend/routes/run.routes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 router.post('/js', async (req, res) => {
     const { code, question_id } = req.body
 
-    res.send({status: "code ran successfully"})
+    res.send({status: "code ran successfully", code, question_id})
 })
 
 module.exports = router 
