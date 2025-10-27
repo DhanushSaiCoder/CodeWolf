7d8f3ff3f9dd3608dfbaf129ba0e767f143178ca
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-10-27 19:43:39 +0530

    ~format

--- backend/routes/submit.routes.js
+++ backend/routes/submit.routes.js
@@ -3,12 +3,11 @@ const router = express.Router()
 const { VM } = require('vm2');
 
 router.post('/js', async (req, res) => {
-    const { code, question } = req.body;
-    console.log("questionDoc: ", question)
-    const testcases = question.test_cases;
+  const { code, question } = req.body;
+  const testcases = question.test_cases;
 
-    // Build the test harness dynamically
-    const harness = `
+  // Build the test harness dynamically
+  const harness = `
     ${code} 
     const testcases = ${JSON.stringify(testcases)};
     const results = [];
@@ -26,19 +25,23 @@ router.post('/js', async (req, res) => {
     results;
   `;
 
-    try {
-        const vm = new VM({ timeout: 1000, sandbox: {} });
-        const results = vm.run(harness);
-        let all_PASS = true;
+  try {
+    const vm = new VM({ timeout: 1000, sandbox: {} });
+    const results = vm.run(harness);
+    let all_PASS = true;
 
-        for(const testcase of results)
-          if(testcase.result != "PASS") all_PASS = false
+    for (const testcase of results)
+      if (testcase.result != "PASS") all_PASS = false
 
-        res.json({ success: true, results, all_PASS });
-    } catch (err) {
-        res.status(500).json({ success: false, error: err.message });
-    }
+    res.json({ success: true, results, all_PASS });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
 });
+
+router.post('/c', async (req, res) => {
+
+})
 
 
 module.exports = router
