012ff6fcc8046c86f1ea9fea38b96d8b9bfd758c
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-11-03 22:46:52 +0530

    redesign match info

--- frontend/src/components/MatchInfo.js
+++ frontend/src/components/MatchInfo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "../styles/MatchInfo.css";
 import Profile from "../images/profile.jpg";
 import { jwtDecode } from 'jwt-decode';
-
+import { Swords } from "lucide-react"
 export default function MatchInfo({ matchDoc }) {
     if (!matchDoc) return <div className='MatchInfo'>Loading...</div>;
 
@@ -25,7 +25,7 @@ export default function MatchInfo({ matchDoc }) {
             <div className='MIPlayerDetails'>
                 {/* Player One */}
                 <div className='MIPlayerOneDetails bothPlayerDetails'>
-                    <img className='profileImgs' src={player_lowQualityPic || Profile} alt="playerOne" width={75} height={75} />
+                    <img className='profileImgs' src={player_lowQualityPic || Profile} alt="playerOne" />
                     <div>
                         <h2>{player?.username || "Unknown"}</h2>
                         <p>★ {player?.rating || "Unknown"}</p>
@@ -34,18 +34,18 @@ export default function MatchInfo({ matchDoc }) {
 
                 {/* VS */}
                 <div className='vsDiv'>
-                    <div className='vsCircle'><p>vs</p></div>
+                    <div className='vsCircle'><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="#d5bcf5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-swords-icon lucide-swords"><polyline points="14.5 17.5 3 6 3 3 6 3 17.5 14.5"/><line x1="13" x2="19" y1="19" y2="13"/><line x1="16" x2="20" y1="16" y2="20"/><line x1="19" x2="21" y1="21" y2="19"/><polyline points="14.5 6.5 18 3 21 3 21 6 17.5 9.5"/><line x1="5" x2="9" y1="14" y2="18"/><line x1="7" x2="4" y1="17" y2="20"/><line x1="3" x2="5" y1="19" y2="21"/></svg></div>
                 </div>
 
                 {/* Player Two */}
                 <div className='MIPlayerTwoDetails bothPlayerDetails '>
-                    <div>
+                    <img className='profileImgs' src={opponent_lowQualityPic || Profile} alt="playerTwo" />
+                    <div className='MIPlayerTwoDetails_details'>
 
                         <h2>{opponent?.username || "Unknown"}</h2>
                         <p>★ {opponent?.rating || "Unknown"}</p>
 
                     </div>
-                    <img className='profileImgs' src={opponent_lowQualityPic || Profile} alt="playerTwo" width={75} height={75} />
                 </div>
             </div>
         </div>
